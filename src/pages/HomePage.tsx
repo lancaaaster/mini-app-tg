ea7b352a11ad0eb243d5a714601d2f9ea5805ec6
lancaaaster <eminensio001@gmail.com>
2025-07-05 18:07:35 +0300

    1.7

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -49,7 +49,7 @@ const HomePage: React.FC = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 pb-24">
       {/* Баннер */}
       <Banner
-        image="https://cdn.easydonate.shop/banner-example.jpg"
+        image=""
         title="Brawl Pass на EasyDonate!"
         subtitle="Покупайте донат для любимых игр быстро и безопасно"
         buttonText="Купить"
